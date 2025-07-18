a375ee434c074c35015dee4b5276b5d991eebec7
junhihi <127943579+junhihi@users.noreply.github.com>
2025-07-18 17:07:17 +0900

    Add CNN model definition with two conv layers and dropout

---
+++ model.py
@@ -0,0 +1,22 @@
+import torch
+import torch.nn as nn
+
+class CNN(nn.Module):
+    def __init__(self):
+        super(CNN, self).__init__()
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.fc1 = nn.Linear(64 * 56 * 56, 64)
+        self.fc2 = nn.Linear(64, 5)
+        self.relu = nn.ReLU()
+        self.dropout = nn.Dropout(0.5)
+
+    def forward(self, x):
+        x = self.pool(self.relu(self.conv1(x)))
+        x = self.pool(self.relu(self.conv2(x)))
+        x = x.view(x.size(0), -1)  # batch_size 동적 유지
+        x = self.relu(self.fc1(x))
+        x = self.dropout(x)
+        x = self.fc2(x)
+        return x
