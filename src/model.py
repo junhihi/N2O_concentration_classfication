09c04dfbe41a703a709508bbeed91fad952df38d
junhihi <127943579+junhihi@users.noreply.github.com>
2025-08-05 14:15:07 +0900

    feat: increased CNN depth and added BatchNormalization after conv layers and set  input chennel to 1

--- src/model.py
+++ src/model.py
@@ -1,22 +1,26 @@
 import torch.nn as nn
 
 class CNN(nn.Module):
-    """이미지 분류를 위한 CNN 모델"""
     def __init__(self, num_classes=1):
         super(CNN, self).__init__()
-        self.conv1 = nn.Conv2d(3, 32, kernel_size=3, padding=1)
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=3, padding=1)
+        self.bn1 = nn.BatchNorm2d(32)
         self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
+        self.bn2 = nn.BatchNorm2d(64)
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, padding=1)  # 추가 층
+        self.bn3 = nn.BatchNorm2d(128)
         self.pool = nn.MaxPool2d(2, 2)
-        self.fc1 = nn.Linear(64 * 56 * 56, 64)
-        self.fc2 = nn.Linear(64, 1)
+        self.global_pool = nn.AdaptiveAvgPool2d((1, 1))  # 고정 크기 출력
+        self.fc1 = nn.Linear(128, 64)
+        self.fc2 = nn.Linear(64, num_classes)
         self.relu = nn.ReLU()
         self.dropout = nn.Dropout(0.5)
 
     def forward(self, x):
-        """모델 순전파"""
-        x = self.pool(self.relu(self.conv1(x)))
-        x = self.pool(self.relu(self.conv2(x)))
-        x = x.view(x.size(0), -1)
+        x = self.pool(self.relu(self.bn1(self.conv1(x))))
+        x = self.pool(self.relu(self.bn2(self.conv2(x))))
+        x = self.pool(self.relu(self.bn3(self.conv3(x))))
+        x = self.global_pool(x).view(x.size(0), -1)
         x = self.relu(self.fc1(x))
         x = self.dropout(x)
         x = self.fc2(x)
